a0d188ac0d92531f7ae382efd5a463555e2f0588
Marcin Leliwa <marcin.leliwa@gmail.com>
2026-01-13 21:30:34 +0100

    v.0.2.1

--- docker_run.sh
+++ docker_run.sh
@@ -2,13 +2,8 @@
 
 source ./config.sh
 
-mkdir -p ./backend/data
 
 docker run --rm \
 -p 8080:8080 \
---env-file ./backend/.env \
---user $(id -u):$(id -g) \
---env UV_CACHE_DIR=/tmp/.cache/uv \
---mount type=bind,source=./backend/data,target=/data \
---name $SERVICE_NAME \
+--name seavor \
 $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
