b5f07eecb150d1998d51ed296eca44e24500d69b
Marcin Leliwa <marcin.leliwa@gmail.com>
2026-02-26 08:49:15 +0100

    feat(frontned) Add markdown libraries and MarkdownPipe
    ```bash
    npm install marked
    npm install marked-highlight
    npm install highlight.js
    ```

---
+++ frontend/src/app/shared/markdown.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import hljs from 'highlight.js';
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+
+const marked = new Marked(
+    markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, lang, info) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+        }
+    })
+);
+
+@Pipe({
+    name: 'markdown',
+    standalone: true
+})
+export class MarkdownPipe implements PipeTransform {
+    transform(value: any, args?: any[]): any {
+        if (value && value.length > 0) {
+            return marked.parse(value);
+        }
+        return value;
+    }
+}
