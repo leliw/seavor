f61fde0fa367040e633e82acc2639c8e4db72e43
Marcin Leliwa <marcin.leliwa@gmail.com>
2026-07-10 09:48:42 +0200

    fix(infra)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,3 +6,10 @@ output "firebase_dns_records" {
   value       =  module.app.firebase_dns_records
   description = "Changest required in DNS records"
 }
+output "service_account_key" {
+  value     = local.create_app ? null : module.app.service_account_key
+  sensitive = true
+}
+output "env_file" {
+  value = module.app.env_file
+}
