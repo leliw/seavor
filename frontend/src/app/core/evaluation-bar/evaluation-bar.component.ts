2ac61228aeb0664e57ab5629134e9ae23c13ce00
Marcin Leliwa <marcin.leliwa@gmail.com>
2026-07-17 12:22:17 +0200

    feat(EvaluationBar): Add translation support
    - Add a translation toggle button to the evaluation bar component
    - Allow users to switch language views during the evaluation phase
    - Expose showTranslate input and translate output in the component

--- frontend/src/app/core/evaluation-bar/evaluation-bar.component.ts
+++ frontend/src/app/core/evaluation-bar/evaluation-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component, output, ChangeDetectionStrategy } from '@angular/core';
+import { Component, output, ChangeDetectionStrategy, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
     selector: 'app-evaluation-bar',
@@ -9,11 +10,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         MatToolbarModule,
         MatListModule,
         MatIconModule,
+        MatTooltipModule,
     ],
     templateUrl: './evaluation-bar.component.html',
     changeDetection: ChangeDetectionStrategy.Eager,
     styleUrl: './evaluation-bar.component.scss',
 })
 export class EvaluationBarComponent {
+    showTranslate = input<boolean>(false);
     evaluation = output<number>();
+    translate = output<void>();
 }
